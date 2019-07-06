3852bbec4cc73bb5bc3063432569049c68822ba4
Jae Wu <jaewuchun@gmail.com>
2019-07-06 16:42:09 -0700

    initialized

---
+++ src/main/java/com/mjtoolbox/newdvdrental/NewdvdrentalApplication.java
@@ -0,0 +1,13 @@
+package com.mjtoolbox.newdvdrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewdvdrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewdvdrentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjtoolbox/newdvdrental/NewdvdrentalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mjtoolbox.newdvdrental;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewdvdrentalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
